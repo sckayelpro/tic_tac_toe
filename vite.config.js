ca17760004ab0823f71bac9e20f66f35934fb4cb
sckayelpro <kevinwilsuareznina98@gmail.com>
2024-05-08 00:31:32 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/tic-tac-toee/"
+  base: "/tic_tac_toe/"
 })
